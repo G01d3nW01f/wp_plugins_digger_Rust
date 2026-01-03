a49df92b0f1a027dbbcc8e4f62348de0be51d855
G01d3nW01f <75846902+G01d3nW01f@users.noreply.github.com>
2026-01-03 18:29:04 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,16 @@ use std::env;
 use url::Url;
 
 fn main() {
-
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
         eprintln!("[!] Require: argument <targetURL>");
         std::process::exit(1);
-
     }
 
     let base_url = &args[1];
-    
 
     let parsed_base = Url::parse(base_url)
         .expect("[!] Invalid URL");
-
 
     let response = get(base_url)
         .expect("[!] Failed to request URL")
@@ -29,7 +25,7 @@ fn main() {
         .expect("Invalid regex");
 
     let mut plugins = HashSet::new();
-    
+
     for cap in re.captures_iter(&response) {
         plugins.insert(cap[1].to_string());
     }
@@ -37,17 +33,40 @@ fn main() {
     let mut plugins: Vec<String> = plugins.into_iter().collect();
     plugins.sort();
 
+    // Changelog 
+    let version_re = Regex::new(r"(?m)^=\s*([\d\.]+)\s*=")
+        .expect("Invalid version regex");
+
     for plugin in plugins {
-        let plugin_path = format!("/wp-content/plugins/{}",plugin);
+        let plugin_path = format!("/wp-content/plugins/{}/", plugin);
         let absolute_url = parsed_base
             .join(&plugin_path)
             .expect("Failed to build URL");
 
-        
-        println!("{}",&absolute_url);
-        println!("  └ Plugin: {}",plugin);
-        println!("[+]Try Access: {}/readme.txt",absolute_url);
-        println!("");
+        println!("{}", absolute_url);
+        println!("  └ Plugin: {}", plugin);
 
+        // readme.txt URL
+        let readme_url = format!("{}readme.txt", absolute_url);
+        println!("[+] Try Access: {}", readme_url);
+
+        match get(&readme_url) {
+            Ok(resp) => {
+                if let Ok(text) = resp.text() {
+                    if let Some(cap) = version_re.captures(&text) {
+                        println!("  └ Version: {}", &cap[1]);
+                    } else {
+                        println!("  └ Version: Not Found");
+                    }
+                } else {
+                    println!("  └ Failed to read readme.txt");
+                }
+            }
+            Err(_) => {
+                println!("  └ readme.txt not accessible");
+            }
+        }
+
+        println!();
     }
 }
